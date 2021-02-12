92ce1cdb7c19e2be522f3ec75fafc2058f986578
Tia Low <lowtia@gmail.com>
2021-02-11 17:15:46 -0800

    More complex data types

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var x = 10;
 var y = 20;
 var sum;
 var title = "Codevolution";
-// --- Basic Data Types ---
+// --- BASIC Data Types ---
 // boolean
 // number
 // string
@@ -17,3 +17,43 @@ var name = 'Tia';
 // template strings
 var sentence = "My name is " + name + "\nI am a beginner in Typescript";
 console.log(sentence);
+// INTELLISENSE
+// type name. or total. and it will come up with list of methods available on those data types
+// --- SUBTYPES of all other data ---
+// can assign null or undefined to boolean, string, number variable
+// null
+// undefined
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+// --- ARRAY Data Types ---
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+// --- TUPLE Data Type  ---
+// issue with the below is it's fixed num of values, order of values has to match order of types
+var person1 = ['Tia', 22];
+// --- ENUM Data Type ---
+// set of numeric values
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c); //1
+(function (Color) {
+    Color[Color["Yellow"] = 5] = "Yellow";
+    Color[Color["Purple"] = 6] = "Purple";
+    Color[Color["Black"] = 7] = "Black";
+})(Color || (Color = {}));
+;
+var p = Color.Purple;
+console.log(p); //6
+// --- ANY Data Type ---
+// expecting value from library, or data from user, not sure of what type
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Tia';
--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ let sum;
 
 const title = "Codevolution";
 
-// --- Basic Data Types ---
+// --- BASIC Data Types ---
 // boolean
 // number
 // string
@@ -24,7 +24,52 @@ let name: string = 'Tia';
 let sentence: string = `My name is ${name}
 I am a beginner in Typescript`;
 
-console.log(sentence)
+console.log(sentence);
+
+// INTELLISENSE
+// type name. or total. and it will come up with list of methods available on those data types
+
+// --- SUBTYPES of all other data ---
+// can assign null or undefined to boolean, string, number variable
+// null
+// undefined
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+// --- ARRAY Data Types ---
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+
+// --- TUPLE Data Type  ---
+// issue with the below is it's fixed num of values, order of values has to match order of types
+
+let person1: [string, number] = ['Tia', 22]
+
+// --- ENUM Data Type ---
+// set of numeric values
+
+enum Color {Red, Green, Blue};
+
+let c: Color = Color.Green;
+console.log(c); //1
+
+enum Color {Yellow = 5, Purple, Black};
+
+let p: Color = Color.Purple;
+console.log(p); //6
+
+// --- ANY Data Type ---
+// expecting value from library, or data from user, not sure of what type
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Tia';
 
 
 
