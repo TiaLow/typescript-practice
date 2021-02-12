76af92f3e602bf8d709b0047c8097021db415333
Tia Low <lowtia@gmail.com>
2021-02-11 17:03:48 -0800

    Basic data types

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 exports.__esModule = true;
 var message = 'Sup! Welcome Back!';
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = "Codevolution";
+// --- Basic Data Types ---
+// boolean
+// number
+// string
+// let isBeginner of type boolean equal true
+var isBeginner = true;
+var total = 0;
+var name = 'Tia';
+// template strings
+var sentence = "My name is " + name + "\nI am a beginner in Typescript";
+console.log(sentence);
--- main.ts
+++ main.ts
@@ -2,3 +2,29 @@ export{}
 let message = 'Sup! Welcome Back!';
 
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+
+const title = "Codevolution";
+
+// --- Basic Data Types ---
+// boolean
+// number
+// string
+
+// let isBeginner of type boolean equal true
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Tia';
+
+// template strings
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence)
+
+
+
